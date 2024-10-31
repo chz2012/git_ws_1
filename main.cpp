1bebcc5a636f67454a5a741493305d2c25353c30
zhangxi <917569774@qq.com>
2024-10-31 20:08:10 +0800

    zhagnxi submmit

--- main.cpp
+++ main.cpp
@@ -2,9 +2,9 @@
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello, zhagnxi!" << std::endl;
     // Git operation 1: You need to fix follow code. Add you name and sutdent id after that.
-    std::cout << "This change frome lql " << std::endl;
+    std::cout << "This change frome zhagnxi " << std::endl;
     // std::cout << "Hello, World!" << std::endl;
     // std::cout << "Hello, World!" << std::endl;
     return 0;
