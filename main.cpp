674e6f80b3dad90baa92d257803f8abd7f55b190
zhangxi <917569774@qq.com>
2024-10-31 21:30:28 +0800

    develop:change to test git push

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ int main()
 {
     std::cout << "Hello, zhagnxi!" << std::endl;
     // Git operation 1: You need to fix follow code. Add you name and sutdent id after that.
-    std::cout << "This change frome zhagnx " << std::endl;
+    std::cout << "This change frome zhagnx " << std::endl;// i nee to do some diff
     // std::cout << "Hello, World!" << std::endl;
     // std::cout << "Hello, World!" << std::endl;
     return 0;
