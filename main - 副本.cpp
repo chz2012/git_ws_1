fe00b21de6055f28357acfb19cf0831e1c84e547
超级王宇 <1785172193@qq.com>
2024-10-31 23:10:59 +0800

    王宇

---
+++ main - 副本.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello, World!" << std::endl;
+    // Git operation 1: You need to fix follow code. Add you name and sutdent id after that.
+    std::cout << "This change frome lql " << std::endl;
+    // std::cout << "Hello, World!" << std::endl;
+    // std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
